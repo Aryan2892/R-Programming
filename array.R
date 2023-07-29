f399af2c6ff4c3830ff2c352cbcedc52a09b3297
Aryan Bhasein <78354139+Aryan2892@users.noreply.github.com>
2023-07-29 23:00:31 +0530

    Basics of arrays

---
+++ array.R
@@ -0,0 +1,17 @@
+greetings <- c("hi","hello","hola","hey")
+
+greetings[1]
+
+greetings[-1]
+
+greetings[c(1,3)]
+
+greetings[c(2,-1)]
+
+greetings[c(TRUE,FALSE,TRUE,FALSE)]
+
+num_vec <- c(5,9,30,0,-8)
+
+#local indexing
+num_vec < 6
+num_vec[num_vec < 6]
