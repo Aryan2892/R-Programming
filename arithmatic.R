9679ed8c2c91850abd4d57932b68f99e48f54676
Aryan Bhasein <78354139+Aryan2892@users.noreply.github.com>
2023-08-03 19:19:57 +0530

    Basic arithmatic operations in R

---
+++ arithmatic.R
@@ -0,0 +1,30 @@
+#Matrix arithmatic
+set.seed(123)
+ 
+#Addition
+#matrix1 + matrix2
+#Puts them element wise in a new matrix
+
+#Subtraction
+#matrix 1 - matrix2
+
+#Division
+#matrix1/Matrix2
+
+#Multiplicxation
+# * = element wise multiplication 
+#matrix1 * matrix2
+
+# %*% = dot(inner) product
+# the number of col must be equal to the no of row in other
+#takes first row and matches with first col of other matrix
+# multiplies them and then finds the sum 
+#matrix1 %*% t(matrix2)
+
+#%o% = outer product
+# multiplies evrything in matix together
+#matrix1 %o% t(matrix2)
+
+#%x% = kronecker product - generalisation of outer product
+#matrix1 %x% t(matrix2)
+
